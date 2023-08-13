7a3d4b779c2b77ef1ef1b16421d51771cecc3d91
chaitanya agashe <chaitanyaagashe75@gmail.com>
2023-08-13 10:33:59 +0530

    commit

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -9,7 +9,9 @@ public class Demo2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 
-		// this is only for commenting purpose 
+		// this is only for commenting purpose
+
+		// changes making 2
 	}
 
 }
